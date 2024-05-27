43c316e5ba05b16972b89b5ac7dc972cfee8cbe9
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-26 19:25:37 -0500

    Creación de la carpeta Views, se agregó una clase espcializada en registro. Dentro de esta se importaron imagenes y se definieron margenes

--- MauiBlogUdlaApp/App.xaml.cs
+++ MauiBlogUdlaApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiBlogUdlaApp
+﻿using MauiBlogUdlaApp.Views;
+
+namespace MauiBlogUdlaApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new PaginaRegistro();
         }
     }
 }
